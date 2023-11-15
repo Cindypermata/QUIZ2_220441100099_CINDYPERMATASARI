d738db8dc6f71d446e5fffd5aea7fc96b29227ff
Cindypermata <113223444+Cindypermata@users.noreply.github.com>
2023-11-16 00:07:03 +0700

    Add files via upload

---
+++ Quiz2/src/quiz2/koneksi.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz2;
+
+import com.mysql.jdbc.Connection;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author asus
+ */
+public class koneksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static Connection mysqlkonek;
+    public static Connection koneksiDB() throws SQLException{
+        try{
+            String db = "jdbc:mysql://localhost:3306/kuisdua";
+            String user = "root";
+            String pw = "";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlkonek = (Connection) DriverManager.getConnection(db, user, pw);
+//            JOptionPane.showMessageDialog(null, "Koneksi Berhasil");
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Koneksi Gagal" + e.getMessage());
+        }
+        return mysqlkonek;
+    }
+    
+    public Connection getConnection(){
+        return mysqlkonek;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        try{
+            java.sql.Connection conn = koneksiDB();
+            if(conn != null){
+//                System.out.println("Koneksi Berhasil");
+            }
+        }catch(Exception e){
+            System.out.println("Terjadi Kesalahan: " + e.getMessage());
+        }
+    }
+    
+}
